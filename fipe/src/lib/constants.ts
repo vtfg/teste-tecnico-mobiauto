710d66c40068893be9d5164cc5c5d1ba360d795f
Victor Guedes <victorfelipe.guedes@hotmail.com>
2025-03-30 18:15:06 -0300

    chore(fipe): change not found error message

--- fipe/src/lib/constants.ts
+++ fipe/src/lib/constants.ts
@@ -7,7 +7,7 @@ type ErrorMessages = {
 export const errorMessages: ErrorMessages = {
   BAD_REQUEST:
     "Você precisa preencher todos os campos antes de consultar o preço.",
-  NOT_FOUND: "Item não encontrado. Realize a pesquisa novamente.",
+  NOT_FOUND: "Carro não encontrado. Realize a pesquisa novamente.",
   TOO_MANY_REQUESTS:
     "Você fez muitas pesquisas em um curto período de tempo. Tente novamente mais tarde.",
   INTERNAL_SERVER_ERROR:
