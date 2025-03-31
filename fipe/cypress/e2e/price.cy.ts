1ea34b08d3f3bf95babd32b7eca58e0802016b3f
Victor Guedes <victorfelipe.guedes@hotmail.com>
2025-03-31 16:05:22 -0300

    test(fipe): create car not found e2e test

--- fipe/cypress/e2e/price.cy.ts
+++ fipe/cypress/e2e/price.cy.ts
@@ -6,4 +6,15 @@ describe("Price", () => {
 
     cy.get("h2").first().should("have.text", "R$ 11.148,00")
   })
+
+  it("should redirect to home page if the car is not found", () => {
+    cy.visit("/price?brand=1&model=1&year=1992-3")
+
+    cy.location("pathname").should("eq", "/")
+
+    cy.get(".MuiAlert-message").should(
+      "have.text",
+      "Carro não encontrado. Realize a pesquisa novamente.",
+    )
+  })
 })
